d8ae990ea983ddf2fc351cdad1a88cab82516f40
mopsterofpuppets <mfa32@hotmail.com>
2025-09-14 18:16:20 +0200

    Add files via upload

---
+++ firebase_c.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+<script>
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3Hud19A9moYfTE-Dxl02aXNYuGZsnrII",
+  authDomain: "densorteskole-384e7.firebaseapp.com",
+  projectId: "densorteskole-384e7",
+  storageBucket: "densorteskole-384e7.firebasestorage.app",
+  messagingSenderId: "760555159403",
+  appId: "1:760555159403:web:6447a6f6c28b3a6034562e",
+  measurementId: "G-DLTFWTTV7T"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
