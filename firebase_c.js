fe8b242c55cae849793daa1024177aa4e818dc6c
mopsterofpuppets <mfa32@hotmail.com>
2025-09-15 16:53:14 +0200

    Add files via upload

--- firebase_c.js
+++ firebase_c.js
@@ -1,17 +1,14 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
-<script>
+// Hent Firebase biblioteker fra CDN
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD3Hud19A9moYfTE-Dxl02aXNYuGZsnrII",
-  authDomain: "densorteskole-384e7.firebaseapp.com",
-  projectId: "densorteskole-384e7",
-  storageBucket: "densorteskole-384e7.firebasestorage.app",
-  messagingSenderId: "760555159403",
-  appId: "1:760555159403:web:6447a6f6c28b3a6034562e",
-  measurementId: "G-DLTFWTTV7T"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3Hud19A9moYfTE-Dxl02aXNYuGZsnrII",
+    authDomain: "densorteskole-384e7.firebaseapp.com",
+    projectId: "densorteskole-384e7",
+    storageBucket: "densorteskole-384e7.firebasestorage.app",
+    messagingSenderId: "760555159403",
+    appId: "1:760555159403:web:6447a6f6c28b3a6034562e",
+    measurementId: "G-DLTFWTTV7T"
+  };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-</script>
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
